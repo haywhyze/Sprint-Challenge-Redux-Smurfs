dd4edd3ff154c1b46ba6da6be1305ad38b513d84
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-21 10:01:44 +0100

    add action types

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,6 +3,12 @@
   Be sure to export each action type so you can pull it into your reducer
 */
 
+export const ADD_SMURF = 'ADD_SMURF';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS'
+export const GET_SMURFS = 'GET_SMURFS';
+export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS';
+export const FETCH_FAILED = 'FAILED';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
